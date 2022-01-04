9291a2be767e35688cd1f5ff20d96dd9e4e7dae3
shubh-am003 <shubhamshastri100@gmail.com>
2022-01-04 13:53:38 +0530

    First commit

---
+++ bs-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "server" : {
+      "baseDir": [
+        "./src",
+        "./build/contracts"
+      ],
+      "routes": {
+        "/vendor": "./node_modules"
+      }
+    }
+  }
